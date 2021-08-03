70caabf2ad20dacc964e773d506e1edf059a119e
Pakorn-assawin <70192843+Pakorn-assawin@users.noreply.github.com>
2021-08-04 06:25:32 +0700

    up

--- aidea2021/settings.py
+++ aidea2021/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(^f3x#ywz4m5q0p8m7^hc%0@iia3mgxsyav)^dxv_$$^*+diu*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = []
 
 
